c4cff5ce4be44787f1d40ff7a20f803737af0469
Pfizz <pfiste80@gmail.com>
2021-08-18 13:25:26 -0400

    set up packages

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+
+// THEN a high-quality, professional README.md is generated with 
+// the title of my project and sections entitled Description, 
+// Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
